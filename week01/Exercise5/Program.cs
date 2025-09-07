e9145f1d073004973dbfa8476b5a1580e009c990
Halcon-lass <halcon.lass@gmail.com>
2025-09-06 18:34:15 -0600

    Finished exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Globalization;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMessage();
+
+        string name = UserName();
+        int number = UserNumber();
+
+        int squaredNumber = SquareNumber(number);
+
+        result(name, squaredNumber);
+
     }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string UserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int UserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+
+        return square;
+    }
+    static void result(string name, int square)
+    {
+        Console.Write($"{name}, the square of your number is: {square} ");
+    }
+
 }
