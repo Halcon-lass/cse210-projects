e048adff3285bc325be2fe2c047d8e85eb96f687
Halcon-lass <halcon.lass@gmail.com>
2025-09-06 18:08:42 -0600

    Finished exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        
+        int guess = -1;
+        int numberOfGuesses = 0;
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            numberOfGuesses += 1;
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You gussed it!");
+                Console.WriteLine($"It took you {numberOfGuesses} guesses. ");
+            
+                Console.WriteLine("Would you like to play again, yes or no? ");
+                string playAgain = Console.ReadLine();
+                
+                if (playAgain == "yes")
+                {
+                    guess = 0;
+                    numberOfGuesses = 0;
+                    number = randomGenerator.Next(1, 101);
+                }
+                else
+                {
+                    Console.WriteLine("Thank you for playing, goodbye!");
+                }
+            }
+                
+
+                
+
+
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,44 @@
 using System;
 
-class Program
+internal class NewBaseType
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers;
+        numbers = new List<int>();
+
+        int responseNumber = -1;
+
+        while (responseNumber != 0)
+        {
+            Console.Write("Enter a number, type 0 when finished: ");
+            string userList = Console.ReadLine();
+
+            responseNumber = int.Parse(userList);
+
+            if (responseNumber != 0)
+            {
+                numbers.Add(responseNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum} ");
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average} ");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
